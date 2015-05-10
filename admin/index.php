0c26800328820b0527019d1146963819e3b06e69
Jay <staticrunescape@gmail.com>
2015-05-10 19:59:29 +0100

    Admin panel
    Added a basic admin panel, page layout isn't setup properly ~ finishing
    now.

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<html>
+<link rel="stylesheet" type="text/css" href="../Style/style.css">
+<body>
+	<div id='admin_panel_text'>
+	Admin panel
+	</div>
+	
+	<center>
+	<div id='admin_panel_insert'>
+	<input type:text placeholder='username' id='admin_username_input'>
+	</div>
+	</center>
+
+	<div class='ban_button'>
+	Ban user
+	</div>
+	
+	<div class='mute_button'>
+	Mute user
+	</div>
+
+<center>
+	<div id='admin_panel_submit'>
+	<input type="submit">
+	</div>
+</center>
+
+
+</body>
