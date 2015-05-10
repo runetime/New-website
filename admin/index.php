5dae7bd1a0b6774c6ae0f463f6d9d6c77c0a2b23
Jay <staticrunescape@gmail.com>
2015-05-10 20:24:36 +0100

    Admin panel #2
    Created page layout around the admin panel - more styling and additions
    to come!

--- admin/index.php
+++ admin/index.php
@@ -1,29 +1,13 @@
 <html>
 <link rel="stylesheet" type="text/css" href="../Style/style.css">
 <body>
-	<div id='admin_panel_text'>
-	Admin panel
-	</div>
+
+<?php
+	include "../includes/navigation.php";
+	?>
 	
-	<center>
-	<div id='admin_panel_insert'>
-	<input type:text placeholder='username' id='admin_username_input'>
-	</div>
-	</center>
-
-	<div class='ban_button'>
-	Ban user
-	</div>
-	
-	<div class='mute_button'>
-	Mute user
-	</div>
-
-<center>
-	<div id='admin_panel_submit'>
-	<input type="submit">
-	</div>
-</center>
-
+<?php
+	include "../includes/admin.php";
+	?>
 
 </body>
---
+++ includes/admin.php
@@ -0,0 +1,29 @@
+	<div class='main_page'>
+		<div class='main_radio_middle'>
+			<div id='main_title'>
+				<div id='admin_panel_text'>
+	Admin panel
+	</div>
+	
+	<center>
+	<div id='admin_panel_insert'>
+	<input type='text' placeholder='username' id='admin_username_input'>
+	</div>
+	</center>
+
+	<div class='admin_button'>
+	Ban user
+	</div>
+	
+	<div class='admin_button'>
+	Mute user
+	</div>
+
+<center>
+	<div id='admin_panel_submit'>
+	<input type="submit">
+	</div>
+</center>
+			</div>
+		</div>
+	</div>
