efcc471b03d7156cefab82b64b1a2392e8ecdeba
astenbal <cqhd.leek@gmail.com>
2015-05-10 20:45:36 +0200

    Reverted Gitfail

--- radio/book.php
+++ radio/book.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 include("connect.php");
-<<<<<<< HEAD
 /*Get data from URL and process it, check if DJ in URL is the currently logged in one*/
-=======
->>>>>>> e1cc3ebf491105d0f5e709949a3a500aa0731bb7
 $time = mysqli_real_escape_string($con, $_GET['time']);
     if($_GET['dj']!=$_SESSION['username']){
         $dj = $_SESSION['username'];
@@ -12,7 +9,6 @@ $time = mysqli_real_escape_string($con, $_GET['time']);
     else{
         $dj = mysqli_real_escape_string($con, $_GET['dj']);
     }
-<<<<<<< HEAD
 /*Start booking time if both time and dj are set*/
 if(!empty($time)&&!empty($dj)){
     /*Alter time to be useable in SQL query*/
@@ -24,7 +20,6 @@ if(!empty($time)&&!empty($dj)){
     $result = mysqli_query($con, $sql);    
         while($row = mysqli_fetch_array($result)){
             /*If already taken cancel and throw error*/
-=======
 if(!empty($time)&&!empty($dj)){
     $time = explode('-', $time);
     $day = $time[0];
@@ -32,15 +27,11 @@ if(!empty($time)&&!empty($dj)){
     $sql = "SELECT * FROM `timetable` WHERE `day`='".$day."' AND `time`='".$time."'";
     $result = mysqli_query($con, $sql);    
         while($row = mysqli_fetch_array($result)){
->>>>>>> e1cc3ebf491105d0f5e709949a3a500aa0731bb7
             if(!empty($row['dj'])&&$row['dj']!=$dj){
                 echo 'This slot has already been taken';
                 }
             else{
-<<<<<<< HEAD
                 /*If the dj is already set for the timeslot delete*/
-=======
->>>>>>> e1cc3ebf491105d0f5e709949a3a500aa0731bb7
                 if($row['dj']==$dj){
                     $sql = "UPDATE `timetable` SET `dj`='' WHERE `day`='".$day."' AND `time`='".$time."'";
                     $result = mysqli_query($con, $sql);
@@ -52,10 +43,7 @@ if(!empty($time)&&!empty($dj)){
                         echo mysqli_error($con);
                     }
                 }
-<<<<<<< HEAD
                 /*If the dj isn't already set for the timeslot put him there*/
-=======
->>>>>>> e1cc3ebf491105d0f5e709949a3a500aa0731bb7
                 else{
                     $sql = "UPDATE `timetable` SET `dj`='".$dj."' WHERE `day`='".$day."' AND `time`='".$time."'";
                     $result = mysqli_query($con, $sql);
--- radio/country.php
+++ radio/country.php
@@ -1,10 +1,6 @@
 <?php
 if(isset($_POST['country'])){
-<<<<<<< HEAD
     $time = 60*60*24*30;
-=======
-    $time = 5;
->>>>>>> e1cc3ebf491105d0f5e709949a3a500aa0731bb7
     $country = $_POST['country'];
     $notembedded = array('CA', 'US');
     if(in_array($country, $notembedded)){
