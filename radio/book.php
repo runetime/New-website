e00abe002bd6ee8c76d987ad2d90ed74884006aa
astenbal <cqhd.leek@gmail.com>
2015-05-16 15:06:49 +0200

    Updated some fails

--- radio/book.php
+++ radio/book.php
@@ -20,44 +20,46 @@ if(!empty($time)&&!empty($dj)){
     $result = mysqli_query($con, $sql);    
         while($row = mysqli_fetch_array($result)){
             /*If already taken cancel and throw error*/
-if(!empty($time)&&!empty($dj)){
-    $time = explode('-', $time);
-    $day = $time[0];
-    $time = $time[1];
-    $sql = "SELECT * FROM `timetable` WHERE `day`='".$day."' AND `time`='".$time."'";
-    $result = mysqli_query($con, $sql);    
-        while($row = mysqli_fetch_array($result)){
-            if(!empty($row['dj'])&&$row['dj']!=$dj){
-                echo 'This slot has already been taken';
-                }
-            else{
-                /*If the dj is already set for the timeslot delete*/
-                if($row['dj']==$dj){
-                    $sql = "UPDATE `timetable` SET `dj`='' WHERE `day`='".$day."' AND `time`='".$time."'";
-                    $result = mysqli_query($con, $sql);
-                    if(mysqli_affected_rows($con)>0){
-                        echo 'Updated successfully :3<br>
-                        <a href="timetable.php">Click here to return</a>';
-                    }
-                    else{
-                        echo mysqli_error($con);
-                    }
-                }
-                /*If the dj isn't already set for the timeslot put him there*/
-                else{
-                    $sql = "UPDATE `timetable` SET `dj`='".$dj."' WHERE `day`='".$day."' AND `time`='".$time."'";
-                    $result = mysqli_query($con, $sql);
-                    if(mysqli_affected_rows($con)>0){
-                        echo 'Updated successfully :3<br>
-                        <a href="timetable.php">Click here to return</a>';
-                    }
-                    else{
-                        echo mysqli_error($con);
-                    }
-                }
-            }
-        }
-    }
+			if(!empty($time)&&!empty($dj)){
+			$time = explode('-', $time);
+			$day = $time[0];
+			$time = $time[1];
+			$sql = "SELECT * FROM `timetable` WHERE `day`='".$day."' AND `time`='".$time."'";
+			$result = mysqli_query($con, $sql);    
+				while($row = mysqli_fetch_array($result)){
+					if(!empty($row['dj'])&&$row['dj']!=$dj){
+						echo 'This slot has already been taken';
+						}
+					else{
+						/*If the dj is already set for the timeslot delete*/
+						if($row['dj']==$dj){
+							$sql = "UPDATE `timetable` SET `dj`='' WHERE `day`='".$day."' AND `time`='".$time."'";
+							$result = mysqli_query($con, $sql);
+							if(mysqli_affected_rows($con)>0){
+								echo 'Updated successfully :3<br>
+								<a href="timetable.php">Click here to return</a>';
+							}
+							else{
+									echo mysqli_error($con);
+							}
+						}
+						/*If the dj isn't already set for the timeslot put him there*/
+						else{
+						$sql = "UPDATE `timetable` SET `dj`='".$dj."' WHERE `day`='".$day."' AND `time`='".$time."'";
+						$result = mysqli_query($con, $sql);
+						if(mysqli_affected_rows($con)>0){
+							echo 'Updated successfully :3<br>
+							<a href="timetable.php">Click here to return</a>';
+						}
+						else{
+							echo mysqli_error($con);
+						}
+					}
+				}
+			}
+		}
+	}
+}
 else{
     echo 'Tralala';
     }
